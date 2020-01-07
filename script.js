98e3760a241cdf8f25aa0dc876d4ca7a068bbaa5
Nick Young <youngnicholas228@gmail.com>
2020-01-07 13:57:40 -0800

    refactored for use without Bootstrap

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 //Bars
-window.onscroll = function() {
-    progressBar()
+window.onscroll = function () {
+	progressBar()
 };
-	let mains = ["mainabout", "mainprojects", "mainskills", "maincontact"];
-	let bars = ["aboutbar", "projectsbar", "skillsbar", "contactbar"]
+let mains = ["mainabout", "mainprojects", "mainskills", "maincontact"];
+let bars = ["aboutbar", "projectsbar", "skillsbar", "contactbar"]
+
 function progressBar() {
 
-	for (let i = 0; i < mains.length; i++){
+	for (let i = 0; i < mains.length; i++) {
 		var winScroll = document.documentElement.scrollTop - document.getElementById('landing').clientHeight;
 		for (let j = 0; j < i; j++) {
 			winScroll -= document.getElementById(mains[j]).clientHeight
 		}
-	    var height = document.getElementById(mains[i]).clientHeight;
-	    var scrolled = (winScroll / height) * 100 - 8;
+		var height = document.getElementById(mains[i]).clientHeight;
+		var scrolled = (winScroll / height) * 100 - 8;
 		if (scrolled > 0) {
-	    	document.getElementById(bars[i]).style.width = scrolled + "%";
-		}
-		else {
+			document.getElementById(bars[i]).style.width = scrolled + "%";
+		} else {
 			document.getElementById(bars[i]).style.width = "0%";
 		}
 	}
@@ -26,13 +26,13 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-	acc[i].addEventListener("click", function() {
+	acc[i].addEventListener("click", function () {
 		this.classList.toggle("active");
 		var panel = this.nextElementSibling;
 		if (panel.style.maxHeight) {
-		panel.style.maxHeight = null;
+			panel.style.maxHeight = null;
 		} else {
-		panel.style.maxHeight = panel.scrollHeight + "px";
+			panel.style.maxHeight = panel.scrollHeight + "px";
 		}
 	});
 }
