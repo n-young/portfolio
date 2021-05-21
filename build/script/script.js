3d7a2968c6e96cd62db78993e7be2ab9bf60ceb4
Nick Young <youngnicholas228@gmail.com>
2021-05-20 18:17:50 -0700

    shitty build mech

---
+++ build/script/script.js
@@ -0,0 +1,57 @@
+// Bars
+window.onscroll = function () {
+	progressBar()
+};
+let mains = ["mainabout", "mainprojects", "mainskills", "maincontact"];
+let bars = ["aboutbar", "projectsbar", "skillsbar", "contactbar"]
+
+function progressBar() {
+	for (let i = 0; i < mains.length; i++) {
+		var winScroll = document.documentElement.scrollTop - document.getElementById('landing').clientHeight;
+		for (let j = 0; j < i; j++) {
+			winScroll -= document.getElementById(mains[j]).clientHeight
+		}
+		var height = document.getElementById(mains[i]).clientHeight;
+		var scrolled = (winScroll / height) * 100 - 8;
+		if (scrolled > 0) {
+			document.getElementById(bars[i]).style.width = scrolled + "%";
+		} else {
+			document.getElementById(bars[i]).style.width = "0%";
+		}
+	}
+}
+
+// Fade in
+$(window).on("load",function() {
+  $(window).scroll(function() {
+    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+    $(".fade").each(function () {
+      /* Check the location of each desired element */
+      var objectBottom = $(this).offset().top + $(this).outerHeight() / 2;
+      
+      /* If the element is completely within bounds of the window, fade it in */
+      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+        $(this).addClass("faded-in")
+      } else { //object goes out of view (scrolling up)
+        $(this).removeClass("faded-in")
+      }
+    });
+  }).scroll(); //invoke scroll-handler on page-load
+});
+
+// Fun
+$(document).ready(function(){
+  $('.fun').each(function () {
+    var letters = $(this).text();
+    console.log(letters)
+    var nHTML = '';
+    for(var letter of letters) {
+      if (letter == ' ') {
+        nHTML += "<span class='fun-letter'>&nbsp;</span>";
+      } else {
+        nHTML += "<span class='fun-letter'>"+letter+"</span>";
+      }
+    }
+    $(this).html(nHTML);
+  })
+})
