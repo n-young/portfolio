0c0127ef11ec8e246bfdf1a032cade4ec756e69c
Nick Young <youngnicholas228@gmail.com>
2022-11-05 10:33:23 -0400

    new website

--- static/script/script.js
+++ static/script/script.js
@@ -1,26 +1,3 @@
-// Bars
-window.onscroll = function () {
-	progressBar()
-};
-let mains = ["mainabout", "mainprojects", "mainskills", "maincontact"];
-let bars = ["aboutbar", "projectsbar", "skillsbar", "contactbar"]
-
-function progressBar() {
-	for (let i = 0; i < mains.length; i++) {
-		var winScroll = document.documentElement.scrollTop - document.getElementById('landing').clientHeight;
-		for (let j = 0; j < i; j++) {
-			winScroll -= document.getElementById(mains[j]).clientHeight
-		}
-		var height = document.getElementById(mains[i]).clientHeight;
-		var scrolled = (winScroll / height) * 100 - 8;
-		if (scrolled > 0) {
-			document.getElementById(bars[i]).style.width = scrolled + "%";
-		} else {
-			document.getElementById(bars[i]).style.width = "0%";
-		}
-	}
-}
-
 // Fade in
 $(window).on("load",function() {
   $(window).scroll(function() {
